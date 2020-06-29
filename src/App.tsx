5567968d8439708be2f019057dc3e6e9862bff77
dipsywong98 <ycwongal@connect.ust.hk>
2020-06-29 23:40:11 +0800

    Updates

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react'
+import Demo from './components/Demo'
+
+const App: FunctionComponent = () => {
+  return (
+    <Demo />
+  )
+}
+
+export default App
